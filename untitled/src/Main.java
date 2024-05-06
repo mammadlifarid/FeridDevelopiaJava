e87946e97b3773e4f2f001cc3acf401c91992eb1
Ferid Memmedli <mammmadlifarid.2008@gmail.com>
2024-05-06 18:53:33 +0400

    init

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Employee {
+    Integer id;
+    String name;
+    String surname;
+    String phone;
+    String address;
+    int salary;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    // Default Constructor
+    Employee() {
+        System.out.println("Default Constructor Called");
+    }
+
+    // Constructor with name parameter
+    Employee(String name) {
+        this.name = name;
+        System.out.println("Name Constructor Called with name: " + name);
+    }
+
+    // Constructor with name and surname parameters
+    Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        System.out.println("Name and Surname Constructor Called with name: " + name + " and surname: " + surname);
+    }
+
+    // Constructor with name, phone, and salary parameters
+    Employee(String name, String phone, int salary) {
+        this.name = name;
+        this.phone = phone;
+        this.salary = salary;
+        System.out.println("Name, Phone, and Salary Constructor Called with name: " + name + ", phone: " + phone + " and salary: " + salary);
     }
 }
+
